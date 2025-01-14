9331027edcf2e599e3ebdd43bb7dec1a6c7de145
Ferdiansyah <ferdiansyah.fr42@gmail.com>
2025-01-14 18:33:57 +0700

    isi html

--- resources/views/kategori/index.blade.php
+++ resources/views/kategori/index.blade.php
@@ -3,9 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>aplikasi manajemen</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
-    
-</body>
-</html>
+    <div class="container">
+        <h1>manajemen data</h1>
+        <div class="nav">
+            <ul>
+                <li><a href="/kategori">Kategori</a></li>
+                <li><a href="/penerbit">penerbit</a></li>
+                <li><a href="/buku">buku</a></li>
+            </ul>
+        </div>
